8927a6df42c0cc5472f92420b01b615da26eccb1
kulbandita <kulbandita@gmail.com>
2017-07-25 16:49:21 +0700

    add isWinner() and chage from isLove() to isAll()

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -19,8 +19,15 @@ public class TennisGame1 implements TennisGame {
         }
     }
 
-    public boolean isLove() {
+    public boolean isAll() {
         if (m_score1 == m_score2) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isWinner() {
+        if (m_score1 >= 4 || m_score2 >= 4) {
             return true;
         }
         return false;
@@ -29,7 +36,7 @@ public class TennisGame1 implements TennisGame {
     public String getScore() {
         String score = "";
         int tempScore = 0;
-        if (isLove()) {
+        if (isAll()) {
             switch (m_score1) {
                 case 0:
                     score = "Love-All";
@@ -44,7 +51,7 @@ public class TennisGame1 implements TennisGame {
                     score = "Deuce";
                     break;
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
+        } else if (isWinner()) {
             int minusResult = m_score1 - m_score2;
             if (minusResult == 1) {
                 score = "Advantage " + player1Name;
